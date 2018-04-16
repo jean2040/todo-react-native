425caec9e5fa94606fb46cb6b9b1af28901a0dbe
Jean Paul Gomez <jeanpaul.gomez@gmail.com>
2018-04-15 23:38:59 -0400

    Redux Action created

---
+++ app/actions/index.js
@@ -0,0 +1,17 @@
+export const DATA_AVAILABLE = 'DATA_AVAILABLE';
+
+//Import the sample data
+import Data from '../instructions.json';
+
+export function getData(){
+    return (dispatch) => {
+
+        //Make API Call
+
+        setTimeout(() => {
+            const data  = Data.instructions;
+            dispatch({type: DATA_AVAILABLE, data:data});
+        }, 2000);
+
+    };
+}
