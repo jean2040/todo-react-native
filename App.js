0fbac4eeb3b880386aa28fc3094e6c452e7a0736
Jean Paul Gomez <jeanpaul.gomez@gmail.com>
2018-04-16 00:04:25 -0400

    Fixed problem with type 'number' supplied to 'CellRender'

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { Provider } from 'react-redux';
+
+import store from './app/store'; //Import the store
+import Home from './app/components/home' //Import the component file
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+        <Provider store={store}>
+            <Home />
+        </Provider>
     );
   }
 }
